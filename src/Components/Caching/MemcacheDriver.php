2e65ab510289450030629f56ecf1cc9ddc2f5c95
vahit <vahit.serif119@gmail.com>
2017-06-01 01:44:31 +0300

    made some updates

---
+++ src/Components/Caching/MemcacheDriver.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Cable\Caching\Memcache;
+
+
+use Cable\Caching\Driver\DriverInterface;
+use Cable\Caching\Driver\FlushableDriverInterface;
+use Cable\Caching\Driver\TimeableDriverInterface;
+
+class MemcacheDriver implements DriverInterface, TimeableDriverInterface, FlushableDriverInterface
+{
+
+    private $memcache;
+
+    public function __construct()
+    {
+        $this->memcache = new \Memcached();
+
+    }
+
+    /**
+     * @param $name
+     * @param null $default
+     * @return mixed
+     */
+    public function get($name, $default = null)
+    {
+        if (false !== ($value = $this->memcache->get($name))) {
+            return $value;
+        }
+
+        return $default;
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function delete($name)
+    {
+        return $this->memcache->delete($name);
+    }
+
+    /**
+     * @return $this
+     */
+    public function flush()
+    {
+         $this->memcache->flush();
+
+         return $this;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @param int $time
+     * @return mixed
+     */
+    public function set($name, $value, $time)
+    {
+        return $this->memcache->set($name, $value, $time);
+    }
+
+    /**
+     * @param array $configs
+     * @return mixed
+     */
+    public function boot(array $configs)
+    {
+        if (!isset($configs['memcache']['servers'])) {
+            $configs['memcache']['servers'][] = [
+                'host' => '127.0.0.1',
+                'port' => 11211
+            ];
+        }
+
+        $servers= $configs['memcache']['servers'];
+
+        foreach ($servers as $server){
+            $this->memcache->addServer(
+                $server['host'],
+                $server['port']
+            );
+        }
+    }
+}
